fda60aae28588a176da38553b40e278adad51391
Hudson <hudson.junior>
2022-02-22 10:22:04 -0300

    eai blz

---
+++ main.py
@@ -0,0 +1,5 @@
+# Escolhar dataset nos sites
+# Modelar uma rede bayseana para resolver:
+# análise descritiva, análise de diagnóstico, análise preditiva e análise prescritiva.
+# Deve ter no minimo 10 variables
+# Modelar 3 cenários de uso na rede bayeaseiananana
