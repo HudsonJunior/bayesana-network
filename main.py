f35b246a65026afac7b0391a70885c85e23bbbab
Hudson <hudson.junior>
2022-02-23 14:49:27 -0300

    adicionando dataset

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
-# Escolhar dataset nos sites
+# Escolhar dataset nos sites #check
 # Modelar uma rede bayseana para resolver:
-# análise descritiva, análise de diagnóstico, análise preditiva e análise prescritiva.
-# Deve ter no minimo 10 variables
+# análise descritiva, análise de diagnóstico, análise preditiva e análise prescritiva. -> ANálise preditiva
+# Deve ter no minimo 10 variables - ja tem graças a deus
 # Modelar 3 cenários de uso na rede bayeaseiananana
+
+
+# Definir probabilidade da idade da mulher (<20, 20-40, >60)
+# Definir probabilidade da educação (1 2 3 4)
+# Definir probabilidade da educação (1 2 3 4)
+
+
+import pandas as pd
+import matplotlib.pyplot as plt # for drawing graphs
+from pomegranate import *
+import math
