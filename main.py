cf6543b844325cc931abd421b8b82ac5dfc4cdc6
Hudson <hudson.junior>
2022-02-25 14:55:15 -0300

    updating git

--- main.py
+++ main.py
@@ -14,3 +14,25 @@ import pandas as pd
 import matplotlib.pyplot as plt # for drawing graphs
 from pomegranate import *
 import math
+
+
+dataset = open('cmc.data', 'r')
+
+datasetLines = dataset.readlines()
+
+newDatasetLines = []
+
+for line in datasetLines:
+    stringLine = line.replace('\n', '').split(',')
+    intLine = [int(x) for x in stringLine]
+    newDatasetLines.append(intLine)
+
+df = pd.DataFrame(newDatasetLines, columns=['Idade M', 'Educação M', 'Educação H', 'Num Filhos', 'Religião M', 'Trabalha M?', 'Ocupação H', 'Q de vida', 'Mídia', 'Método C'])
+print(df['Idade M'].min())
+
+pIdadeM16 = 0
+pIdadeM27 = 0
+pIdadeM38 = 0
+
+
+print(df.query('`Idade M` >= 16 and `Idade M` < 27'))
