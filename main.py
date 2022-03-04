75ee22a3c38c953c3a898b3295608a24655d2925
Hudson <hudson.junior>
2022-03-04 17:45:16 -0300

    Co-authored-by: Eduardo F Ribeiro <edu-fr@users.noreply.github.com>

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 #
 # 1. Wife's age (numerical)
 # Idade M (16-26, 27-37, 38-49)
-
+            #0       1       2
 # 2. Wife's education (categorical) 1=low, 2, 3, 4=high
 # Educação M (1, 2, 3, 4)
 
@@ -11,7 +11,7 @@
 
 # 4. Number of children ever born (numerical)
 # Num Filhos (0, 1, 2, 3+)
-
+              #0  1  2  3
 # 5. Wife's religion (binary) 0=Non-Islam, 1=Islam
 # Religião M (0, 1)
 
@@ -35,6 +35,7 @@ import matplotlib.pyplot as plt # for drawing graphs
 from pomegranate import *
 import math
 
+pd.set_option('display.max_rows', 500)
 
 def calculaProbIndependente(lista_query, df):
     lista_result = []
@@ -44,6 +45,32 @@ def calculaProbIndependente(lista_query, df):
 
 # def calProbDependent(A, B):
 
+def convertIdadeMulher(df):
+    newIdadeList = []
+    for idade in df['Idade M']:
+        if(idade >= 16 and idade <= 26 ):
+            newIdadeList.append(0)
+
+        if(idade >= 27 and idade <= 37 ):
+            newIdadeList.append(1)
+
+        if(idade >= 38 and idade <= 49 ):
+            newIdadeList.append(2)
+
+    df['Idade M'].update(pd.Series(newIdadeList))
+
+    return df
+
+def convertNumFilhos(df):
+    newNumFilhosList = []
+    for numFilhos in df['Num Filhos']:
+        if(numFilhos >= 3):
+            newNumFilhosList.append(3)
+        else: newNumFilhosList.append(numFilhos)
+
+    df['Num Filhos'].update(pd.Series(newNumFilhosList))
+
+    return df
 
 def main():
     dataset = open('cmc.data', 'r')
@@ -60,33 +87,59 @@ def main():
     df = pd.DataFrame(newDatasetLines, columns=['Idade M', 'Educação M', 'Educação H', 'Num Filhos', 'Religião M',
                                                 'Trabalha M?', 'Ocupação H', 'Q de vida', 'Mídia', 'Método C'])
 
+    df = convertIdadeMulher(df)
+    df = convertNumFilhos(df)
+
     # queries
-    qIdade = ['`Idade M` >= 16 and `Idade M` <= 26', '`Idade M` >= 27 and `Idade M` <= 37',
-              '`Idade M` >= 38 and `Idade M` <= 49']
-    qEducacaoM = ['`Educação M` == 1', '`Educação M` == 2', '`Educação M` == 3', '`Educação M` == 4']
-    qEducacaoH = ['`Educação H` == 1', '`Educação H` == 2', '`Educação H` == 3', '`Educação H` == 4']
-    qNumFilhos = ['`Num Filhos` == 0', '`Num Filhos` == 1', '`Num Filhos` == 2', '`Num Filhos` >= 3']
-    qReligiaoM = ['`Religião M` == 0', '`Religião M` == 1']
-    qTrabalhaM = ['`Trabalha M?` == 0', '`Trabalha M?` == 1']
-    qOcupacaoH = ['`Ocupação H` == 0', '`Ocupação H` == 1', '`Ocupação H` == 2', '`Ocupação H` == 3']
-    qQualDeVida = ['`Q de vida` == 1', '`Q de vida` == 2', '`Q de vida` == 3', '`Q de vida` == 4']
-    qMidia = ['`Mídia` == 0', '`Mídia` == 1']
-    qMetodoC =  ['`Método C` == 1', '`Método C` == 2', '`Método C` == 3']
+    # qIdade = ['`Idade M` >= 16 and `Idade M` <= 26', '`Idade M` >= 27 and `Idade M` <= 37',
+    #           '`Idade M` >= 38 and `Idade M` <= 49']
+    # qEducacaoM = ['`Educação M` == 1', '`Educação M` == 2', '`Educação M` == 3', '`Educação M` == 4']
+    # qEducacaoH = ['`Educação H` == 1', '`Educação H` == 2', '`Educação H` == 3', '`Educação H` == 4']
+    # qNumFilhos = ['`Num Filhos` == 0', '`Num Filhos` == 1', '`Num Filhos` == 2', '`Num Filhos` >= 3']
+    # qReligiaoM = ['`Religião M` == 0', '`Religião M` == 1']
+    # qTrabalhaM = ['`Trabalha M?` == 0', '`Trabalha M?` == 1']
+    # qOcupacaoH = ['`Ocupação H` == 0', '`Ocupação H` == 1', '`Ocupação H` == 2', '`Ocupação H` == 3']
+    # qQualDeVida = ['`Q de vida` == 1', '`Q de vida` == 2', '`Q de vida` == 3', '`Q de vida` == 4']
+    # qMidia = ['`Mídia` == 0', '`Mídia` == 1']
+    # qMetodoC =  ['`Método C` == 1', '`Método C` == 2', '`Método C` == 3']
 
-    # dale
-    probQidade = calculaProbIndependente(qIdade, df)
-    probQEducacaoM = calculaProbIndependente(qEducacaoM, df)
-    probQEducacaoH = calculaProbIndependente(qEducacaoH, df)
-    probQNumFilhos = calculaProbIndependente(qNumFilhos, df)
-    probReligiaoM = calculaProbIndependente(qReligiaoM, df)
-    probQTrabalhaM = calculaProbIndependente(qTrabalhaM, df)
-    probQOcupacaoH = calculaProbIndependente(qOcupacaoH, df)
-    prodQQualDeVida = calculaProbIndependente(qQualDeVida, df) 
-    probQMidia = calculaProbIndependente(qMidia, df)
-    probQMetodoC = calculaProbIndependente(qMetodoC, df)
+    # # dale
+    # probQidade = calculaProbIndependente(qIdade, df)
+    # probQEducacaoM = calculaProbIndependente(qEducacaoM, df)
+    # probQEducacaoH = calculaProbIndependente(qEducacaoH, df)
+    # probQNumFilhos = calculaProbIndependente(qNumFilhos, df)
+    # probReligiaoM = calculaProbIndependente(qReligiaoM, df)
+    # probQTrabalhaM = calculaProbIndependente(qTrabalhaM, df)
+    # probQOcupacaoH = calculaProbIndependente(qOcupacaoH, df)
+    # prodQQualDeVida = calculaProbIndependente(qQualDeVida, df) 
+    # probQMidia = calculaProbIndependente(qMidia, df)
+    # probQMetodoC = calculaProbIndependente(qMetodoC, df)
 
-    print("idade: {}\neduca m: {}\neduca h: {}\nnum filhos: {}\nreligiao m: {}\ntrabalha m: {}\nocupacao h: {}\nqual de vida: {}\nmidia: {}\nmetodo c: {}\n".format(probQidade, probQEducacaoM, probQEducacaoH, probQNumFilhos, probReligiaoM, probQTrabalhaM, probQOcupacaoH, prodQQualDeVida, probQMidia, probQMetodoC))
+    # print("idade: {}\neduca m: {}\neduca h: {}\nnum filhos: {}\nreligiao m: {}\ntrabalha m: {}\nocupacao h: {}\nqual de vida: {}\nmidia: {}\nmetodo c: {}\n".format(probQidade, probQEducacaoM, probQEducacaoH, probQNumFilhos, probReligiaoM, probQTrabalhaM, probQOcupacaoH, prodQQualDeVida, probQMidia, probQMetodoC))
 
-    
-    religiaoMDistribuicao = DiscreteDistribution({})
+    educacaoH = df.groupby('Mídia')['Educação H'].value_counts(normalize=True)
+    numFilhos = df.groupby(['Educação M', 'Idade M'])['Num Filhos'].value_counts(normalize=True)
+    midia = df.groupby(['Religião M', 'Mídia'])['Educação M'].value_counts(normalize=True)
+    educacaoH = df.groupby('Mídia')['Educação H'].value_counts(normalize=True)
+    ocupacaoH = df.groupby('Educação H')['Ocupação H'].value_counts(normalize=True)
+    qDeVida = df.groupby(['Ocupação H', 'Trabalha M?'])['Q de vida'].value_counts(normalize=True)
+    trabalhoM =  df.groupby(['Religião M', 'Educação M'])['Trabalha M?'].value_counts(normalize=True)
+    metodoC = df.groupby(['Q de vida', 'Educação M', 'Num Filhos'])['Método C'].value_counts(normalize=True)
+
+    # Building the bayesian network
+    network = BayesianNetwork("Método contraceptivo")
+
+    d1 = State(midia, "Exposição na mídia")
+    d2 = State(idadeM, "Idade da mulher")
+    d3 = State(ReligiaoM, "Religião da mulher")
+    d4 = State(EducacaoM, "Educação da mulher")
+    d5 = State(midia, "Exposição na mídia")
+    d6 = State(midia, "Exposição na mídia")
+    d7 = State(midia, "Exposição na mídia")
+    d8 = State(midia, "Exposição na mídia")
+    d9 = State(midia, "Exposição na mídia")
+
+    network.add_states()
+
+    # religiaoMDistribuicao = DiscreteDistribution({})
 main()
