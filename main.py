0568ee95b7fb3fc038297d43a539161db114cceb
Eduardo F Ribeiro <ra99571@uem.br>
2022-03-06 20:08:46 -0300

    hudosn

--- main.py
+++ main.py
@@ -37,6 +37,10 @@ import matplotlib.pyplot as plt  # for drawing graphs
 from pomegranate import *
 import math
 
+from pomegranate.distributions import *
+from pomegranate.distributions.ConditionalProbabilityTable import ConditionalProbabilityTable
+from pomegranate.distributions.DiscreteDistribution import DiscreteDistribution
+
 pd.set_option('display.max_rows', 500)
 
 
@@ -117,7 +121,7 @@ def convertTableToString(df):
     listaAux = []
 
     for metodo in df['Método C']:
-        listaAux.append(str(qDeVida))
+        listaAux.append(str(metodo))
     df['Método C'].update(pd.Series(listaAux))
 
     return df
@@ -145,8 +149,7 @@ def getConditionalProbabilyWithThreeParents(items):
     list = []
 
     for i in items:
-        list.append([str(i[0][0]), str(i[0][1]),
-                    str(i[0][2]), str(i[0][3]), i[1]])
+        list.append([str(i[0][0]), str(i[0][1]), str(i[0][2]), str(i[0][3]), i[1]])
 
     return list
 
@@ -180,16 +183,15 @@ def main():
     probQidade = calculaProbIndependente(qIdade, df)
     probQMidia = calculaProbIndependente(qMidia, df)
     probReligiaoM = calculaProbIndependente(qReligiaoM, df)
-    network = BayesianNetwork(
-        "Método contraceptivo")
+    network = BayesianNetwork("Método contraceptivo")
 
     # variáveis discretas independentes
     idadeM = DiscreteDistribution(
         {'0': probQidade[0], '1': probQidade[1], '2': probQidade[2]})
     midia = DiscreteDistribution(
-        {'0': probQMidia[0], '1': probQMidia[1], '2': probQMidia[0]})
+        {'0': probQMidia[0], '1': probQMidia[1]})
     religiaoM = DiscreteDistribution(
-        {'0': probReligiaoM[0], '1': probReligiaoM[1], '2': probReligiaoM[0]})
+        {'0': probReligiaoM[0], '1': probReligiaoM[1]})
     # variáveis dependentes
 
     # Edução do homem
@@ -210,7 +212,7 @@ def main():
 
     educacaoM = ConditionalProbabilityTable(
         educacaoMTable, [religiaoM, midia])
-    print(educacaoM)
+
     # Número de filhos
     numFilhosTable = getConditionalProbabilyWithTwoParents(df.groupby(['Educação M', 'Idade M'])[
         'Num Filhos'].value_counts(normalize=True).iteritems())
@@ -227,53 +229,59 @@ def main():
     qDeVidaTable = getConditionalProbabilyWithTwoParents(df.groupby(['Ocupação H', 'Trabalha M?'])[
         'Q de vida'].value_counts(normalize=True).iteritems())
     qDeVida = ConditionalProbabilityTable(qDeVidaTable, [ocupacaoH, trabalhoM])
+
     # Método contraceptivo
     metodoCTable = getConditionalProbabilyWithThreeParents(df.groupby(['Q de vida', 'Educação M', 'Num Filhos'])[
         'Método C'].value_counts(normalize=True).iteritems())
+    metodoC = ConditionalProbabilityTable(metodoCTable, [qDeVida, educacaoM, numFilhos])
 
-    metodoC = ConditionalProbabilityTable(
-        metodoCTable, [qDeVida, educacaoM, numFilhos])
+    idadeMulherNode = State(idadeM, name="Idade M")
+    midiaNode = State(midia, name="Mídia")
+    religiaoNode = State(religiaoM, name="Religião M")
+    educacaoHomemNode = State(educacaoH, name="Educação H")
+    numFilhosNode = State(numFilhos, name="Num Filhos")
+    educacaoMulherNode = State(educacaoM, name="Educação M")
+    ocupacaoHomemNode = State(ocupacaoH, name="Ocupação H")
+    qDeVidaNode = State(qDeVida, name="Q de vida")
+    trabalhoMNode = State(trabalhoM, name="Trabalha M?")
+    metodoCNode = State(metodoC, name="Método C")
 
-    idadeMulherNode = State(idadeM, name="Idade da mulher")
-    midiaNode = State(midia, name="Exposição na mídia")
-    religiaoNode = State(religiaoM, name="Religião da mulher")
-    educacaoHomemNode = State(educacaoH, name='Educação H')
-    numFilhosNode = State(numFilhos, name='Num Filhos')
-    educacaoMulherNode = State(educacaoM, name='Educação M')
-    ocupacaoHomemNode = State(ocupacaoH, name='Ocupação H')
-    qDeVidaNode = State(qDeVida, name='Qualidade de vida')
-    trabalhoMNode = State(trabalhoM, name='Trabalha M?')
-    metodoCNode = State(metodoC, name='Método Contraceptivo')
+    # Estados
+    network.add_states(metodoCNode, idadeMulherNode, educacaoMulherNode, numFilhosNode, midiaNode, religiaoNode,
+                       educacaoHomemNode, ocupacaoHomemNode, trabalhoMNode, qDeVidaNode)
 
-    network.add_states(idadeMulherNode, educacaoMulherNode,
-                       numFilhosNode)
+    # # Nó - Educação da mulher
+    network.add_edge(midiaNode, educacaoMulherNode)
+    network.add_edge(religiaoNode, educacaoMulherNode)
+
     # Nó - Num de filhos
     network.add_edge(educacaoMulherNode, numFilhosNode)
     network.add_edge(idadeMulherNode, numFilhosNode)
 
-    # # Nó - Educação da mulher
-    # network.add_edge(midiaNode, educacaoMulherNode)
-    # network.add_edge(religiaoNode, educacaoMulherNode)
+    # # Nó - Mulher trabalha?
+    network.add_edge(educacaoMulherNode, trabalhoMNode)
+    network.add_edge(religiaoNode, trabalhoMNode)
 
     # # Nó - Educação do marido
-    # network.add_edge(midiaNode, educacaoHomemNode)
+    network.add_edge(midiaNode, educacaoHomemNode)
 
     # # Nó - Ocupação do marido
-    # network.add_edge(educacaoHomemNode, ocupacaoHomemNode)
-
-    # # Nó - Mulher trabalha?
-    # network.add_edge(educacaoMulherNode, trabalhoMNode)
-    # network.add_edge(religiaoNode, trabalhoMNode)
+    network.add_edge(educacaoHomemNode, ocupacaoHomemNode)
 
     # # Nó - índice do padrão de vida
-    # network.add_edge(ocupacaoHomemNode, qDeVidaNode)
-    # network.add_edge(trabalhoMNode, qDeVidaNode)
+    network.add_edge(ocupacaoHomemNode, qDeVidaNode)
+    network.add_edge(trabalhoMNode, qDeVidaNode)
 
     # # Nó - Método contraceptivo usado
-    # network.add_edge(qDeVidaNode, metodoCNode)
-    # network.add_edge(educacaoMulherNode, metodoCNode)
-    # network.add_edge(numFilhosNode, metodoCNode)
+    network.add_edge(educacaoMulherNode, metodoCNode)
+    network.add_edge(numFilhosNode, metodoCNode)
+    network.add_edge(qDeVidaNode, metodoCNode)
     network.bake()
+
+    beliefs = network.predict_proba({'Educação M': '1'})
+    beliefs = map(str, beliefs)
+    print("n".join("{} {}".format( state.name, belief ) for state, belief in zip (network.states, beliefs)))
+
 
 
 main()
